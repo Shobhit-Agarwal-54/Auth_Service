409c58c4421e3f53acc90f57213050fb39f6775a
Shobhit Agarwal <shobhitagarwal2003@gmail.com>
2024-05-25 17:50:54 +0530

    Added jwt create and verify logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser=require("body-parser");
 const {PORT}=require("./config/serverConfig");
 const apiRoutes=require("./routes/index");
 
+// const UserService=require("./services/user-service");
+
 const app=express();
 
 const prepareAndStartServer=()=>{
@@ -15,6 +17,18 @@ const prepareAndStartServer=()=>{
     
     app.listen(PORT,()=>{
         console.log(`Server Started on Port:${PORT}`);
+        
+    // Testing the JWT token creation and verification 
+     // const service=new UserService();
+    //    const newToken= service.createToken({
+    //         email:"sanket@admin.com",
+    //         id:1
+    //     });
+    //     console.log("New token is ",newToken);
+    // const token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNzE2NjM5MzY3LCJleHAiOjE3MTY2NDI5Njd9.NlKHxRGlmoM2IlOjBpPvnMFGlEnX9cROlVmWVbsVQNY"
+    //    const response= service.verifyToken(token);
+    //    console.log(response);
+
     })
 }
 
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,6 @@
 const UserRepository=require("../repository/user-repository");
+const jwt=require("jsonwebtoken");
+const {JWT_KEY}=require("../config/serverConfig");
 
 class UserService{
     constructor()
@@ -13,6 +15,32 @@ class UserService{
             return user;
         } catch (error) {
             console.log("Something went wrong in the service layer");
+            throw error;
+        }
+    }
+    createToken(user)
+    {
+        try {   
+            // JWT_KEY is a key that is unique for each token creation and is specified by us
+        const result= jwt.sign(user,JWT_KEY,
+            {expiresIn:"1h"}
+        );
+        return result;
+        } catch (error) {
+            console.log("Something went wrong in token creation");
+            throw error;
+        }
+    }
+
+    verifyToken(token)
+    {
+        try {
+           const response= jwt.verify(token,JWT_KEY);
+        // The above line of code will give us the object which was contained inside the token
+        // That is the user object which led to creation of token in the first place
+           return response;
+        } catch (error) {
+            console.log("Something went wrong in token validation",error);
             throw error;
         }
     }
